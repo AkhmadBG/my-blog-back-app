840ccabe20f54a75385bb2c93fa6e2277ef0a3ef
AkhmadBG <akhmadbakir@yandex.ru>
2026-06-18 09:36:08 +0300

    add: tests

--- src/test/java/ru/yandex/practicum/repository/impl/CommentRepositoryImplTest.java
+++ src/test/java/ru/yandex/practicum/repository/impl/CommentRepositoryImplTest.java
@@ -2,36 +2,212 @@ package ru.yandex.practicum.repository.impl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.jdbc.test.autoconfigure.JdbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
-import ru.yandex.practicum.configuration.DataSourceConfiguration;
+import ru.yandex.practicum.dto.NewCommentRequest;
+import ru.yandex.practicum.dto.UpdateCommentRequest;
+import ru.yandex.practicum.entity.Comment;
+import ru.yandex.practicum.mapper.CommentRowMapper;
 
-@SpringBootTest
-@Import(DataSourceConfiguration.class)
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@JdbcTest
+@Import({CommentRepositoryImpl.class, CommentRowMapper.class})
 class CommentRepositoryImplTest {
+
+    @Autowired
+    private CommentRepositoryImpl commentRepository;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+
     @Test
-    void findAllByPostId() {
+    void shouldReturnComments() {
+        jdbcTemplate.update("""
+                        INSERT INTO posts(title, text)
+                        VALUES (?, ?)
+                        """,
+                "title",
+                "text"
+        );
+
+        Long postId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM posts", Long.class);
+
+        jdbcTemplate.update(
+                "INSERT INTO comments(text, post_id) VALUES (?, ?)",
+                "comment1", postId
+        );
+
+        jdbcTemplate.update(
+                "INSERT INTO comments(text, post_id) VALUES (?, ?)",
+                "comment2", postId
+        );
+
+        jdbcTemplate.update(
+                "INSERT INTO comments(text, post_id) VALUES (?, ?)",
+                "comment3", postId
+        );
+
+        List<Comment> comments = commentRepository.findAllByPostId(postId);
+
+        assertThat(comments)
+                .hasSize(3);
     }
 
     @Test
-    void findByPostIdAndCommentId() {
+    void shouldFindCommentsByPostId() {
+        jdbcTemplate.update("""
+                        INSERT INTO posts(title, text)
+                        VALUES (?, ?)
+                        """,
+                "title",
+                "text"
+        );
+
+        Long postId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM posts", Long.class);
+
+        jdbcTemplate.update(
+                "INSERT INTO comments(text, post_id) VALUES (?, ?)",
+                "comment1", postId
+        );
+
+        jdbcTemplate.update(
+                "INSERT INTO comments(text, post_id) VALUES (?, ?)",
+                "comment2", postId
+        );
+
+        jdbcTemplate.update(
+                "INSERT INTO comments(text, post_id) VALUES (?, ?)",
+                "comment3", postId
+        );
+
+        List<Comment> comments = commentRepository.findAllByPostId(postId);
+
+        assertThat(comments)
+                .hasSize(3);
+        assertThat(comments.get(0).getText())
+                .isEqualTo("comment1");
+        assertThat(comments.get(1).getText())
+                .isEqualTo("comment2");
+        assertThat(comments.get(2).getText())
+                .isEqualTo("comment3");
     }
 
     @Test
-    void addComment() {
+    void shouldReturnCommentByPostIdAndCommentId() {
+        jdbcTemplate.update("""
+                        INSERT INTO posts(title, text)
+                        VALUES (?, ?)
+                        """,
+                "title",
+                "text"
+        );
+
+        Long postId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM posts", Long.class);
+
+        jdbcTemplate.update(
+                "INSERT INTO comments(text, post_id) VALUES (?, ?)",
+                "comment1", postId
+        );
+
+        Long commentId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM comments", Long.class);
+
+        Comment comment = commentRepository.findByPostIdAndCommentId(postId, commentId);
+
+        assertThat(comment.getText())
+                .isEqualTo("comment1");
     }
 
     @Test
-    void updateComment() {
+    void shouldAddComment() {
+        jdbcTemplate.update("""
+                        INSERT INTO posts(title, text)
+                        VALUES (?, ?)
+                        """,
+                "title",
+                "text"
+        );
+
+        Long postId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM posts", Long.class);
+
+        NewCommentRequest request = new NewCommentRequest("text", postId);
+
+        Comment comment = commentRepository.addComment(postId, request);
+
+        assertThat(comment)
+                .isNotNull();
+        assertThat(comment.getText())
+                .isEqualTo("text");
+        assertThat(comment.getPostId())
+                .isEqualTo(postId);
     }
 
     @Test
-    void deleteComment() {
+    void shouldUpdateComment() {
+        jdbcTemplate.update("""
+                        INSERT INTO posts(title, text)
+                        VALUES (?, ?)
+                        """,
+                "title",
+                "text"
+        );
+
+        Long postId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM posts", Long.class);
+
+        jdbcTemplate.update(
+                "INSERT INTO comments(text, post_id) VALUES (?, ?)",
+                "comment1", postId
+        );
+
+        Long commentId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM comments", Long.class);
+
+        UpdateCommentRequest updateComment = new UpdateCommentRequest(commentId, "update comment1", postId);
+
+        Comment comment = commentRepository.updateComment(postId, commentId, updateComment);
+
+        assertThat(comment.getText())
+                .isEqualTo("update comment1");
+    }
+
+    @Test
+    void shouldDeleteComment() {
+        jdbcTemplate.update("""
+                        INSERT INTO posts(title, text)
+                        VALUES (?, ?)
+                        """,
+                "title",
+                "text"
+        );
+
+        Long postId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM posts", Long.class);
+
+        jdbcTemplate.update("""
+                        INSERT INTO comments(text, post_id)
+                        VALUES (?, ?)
+                        """,
+                "comment1",
+                postId
+        );
+
+        Long commentId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM comments", Long.class);
+
+        commentRepository.deleteComment(postId, commentId);
+
+        Integer count = jdbcTemplate.queryForObject("""
+                        SELECT COUNT(*)
+                        FROM comments
+                        WHERE id = ?
+                        """,
+                Integer.class,
+                commentId
+        );
+
+        assertThat(count).isZero();
     }
 
 }
--- src/test/java/ru/yandex/practicum/service/impl/CommentServiceImplTest.java
+++ src/test/java/ru/yandex/practicum/service/impl/CommentServiceImplTest.java
@@ -1,45 +1,257 @@
 package ru.yandex.practicum.service.impl;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.yandex.practicum.configuration.CommentServiceTestConfiguration;
+import ru.yandex.practicum.dto.CommentResponse;
+import ru.yandex.practicum.dto.NewCommentRequest;
+import ru.yandex.practicum.dto.UpdateCommentRequest;
+import ru.yandex.practicum.entity.Comment;
 import ru.yandex.practicum.mapper.CommentMapper;
 import ru.yandex.practicum.repository.CommentRepository;
+import ru.yandex.practicum.service.CommentService;
+import ru.yandex.practicum.service.PostService;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = CommentServiceTestConfiguration.class)
 class CommentServiceImplTest {
 
-    @Mock
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
     private CommentRepository commentRepository;
 
-    @Mock
+    @Autowired
+    private PostService postService;
+
+    @Autowired
     private CommentMapper commentMapper;
 
-    @InjectMocks
-    private CommentServiceImpl commentService;
-
-
-
-    @Test
-    void getComments() {
+    @BeforeEach
+    void resetMocks() {
+        Mockito.reset(
+                commentRepository,
+                postService,
+                commentMapper
+        );
     }
 
     @Test
-    void getComment() {
+    void shouldGetComments() {
+
+        Comment comment1 = Comment.builder()
+                .id(1L)
+                .text("comment1")
+                .postId(10L)
+                .build();
+
+        Comment comment2 = Comment.builder()
+                .id(2L)
+                .text("comment2")
+                .postId(10L)
+                .build();
+
+        CommentResponse response1 =
+                new CommentResponse(
+                        1L,
+                        "comment1",
+                        10L
+                );
+
+        CommentResponse response2 =
+                new CommentResponse(
+                        2L,
+                        "comment2",
+                        10L
+                );
+
+        when(commentRepository.findAllByPostId(10L))
+                .thenReturn(List.of(comment1, comment2));
+
+        when(commentMapper.mapToCommentResponse(comment1))
+                .thenReturn(response1);
+
+        when(commentMapper.mapToCommentResponse(comment2))
+                .thenReturn(response2);
+
+        List<CommentResponse> result =
+                commentService.getComments(10L);
+
+        assertEquals(2, result.size());
+
+        assertEquals(response1, result.get(0));
+        assertEquals(response2, result.get(1));
+
+        verify(commentRepository)
+                .findAllByPostId(10L);
     }
 
     @Test
-    void addComment() {
+    void shouldGetComment() {
+
+        Comment comment = Comment.builder()
+                .id(1L)
+                .text("comment")
+                .postId(10L)
+                .build();
+
+        CommentResponse response =
+                new CommentResponse(
+                        1L,
+                        "comment",
+                        10L
+                );
+
+        when(commentRepository.findByPostIdAndCommentId(
+                10L,
+                1L))
+                .thenReturn(comment);
+
+        when(commentMapper.mapToCommentResponse(comment))
+                .thenReturn(response);
+
+        CommentResponse result =
+                commentService.getComment(
+                        10L,
+                        1L
+                );
+
+        assertEquals(response, result);
+
+        verify(commentRepository)
+                .findByPostIdAndCommentId(
+                        10L,
+                        1L
+                );
     }
 
     @Test
-    void updateComment() {
+    void shouldAddComment() {
+
+        NewCommentRequest request =
+                new NewCommentRequest(
+                        "new comment",
+                        10L
+                );
+
+        Comment comment = Comment.builder()
+                .id(1L)
+                .text("new comment")
+                .postId(10L)
+                .build();
+
+        CommentResponse response =
+                new CommentResponse(
+                        1L,
+                        "new comment",
+                        10L
+                );
+
+        when(commentRepository.addComment(
+                10L,
+                request))
+                .thenReturn(comment);
+
+        when(commentMapper.mapToCommentResponse(comment))
+                .thenReturn(response);
+
+        CommentResponse result =
+                commentService.addComment(
+                        10L,
+                        request
+                );
+
+        assertEquals(response, result);
+
+        verify(commentRepository)
+                .addComment(10L, request);
+
+        verify(postService)
+                .incrementOrDecrementPostCommentsCount(
+                        10L,
+                        true
+                );
     }
 
     @Test
-    void deleteComment() {
+    void shouldUpdateComment() {
+
+        UpdateCommentRequest request =
+                new UpdateCommentRequest(
+                        1L,
+                        "updated",
+                        10L
+                );
+
+        Comment comment = Comment.builder()
+                .id(1L)
+                .text("updated")
+                .postId(10L)
+                .build();
+
+        CommentResponse response =
+                new CommentResponse(
+                        1L,
+                        "updated",
+                        10L
+                );
+
+        when(commentRepository.updateComment(
+                10L,
+                1L,
+                request))
+                .thenReturn(comment);
+
+        when(commentMapper.mapToCommentResponse(comment))
+                .thenReturn(response);
+
+        CommentResponse result =
+                commentService.updateComment(
+                        10L,
+                        1L,
+                        request
+                );
+
+        assertEquals(response, result);
+
+        verify(commentRepository)
+                .updateComment(
+                        10L,
+                        1L,
+                        request
+                );
+    }
+
+    @Test
+    void shouldDeleteComment() {
+
+        commentService.deleteComment(
+                10L,
+                1L
+        );
+
+        verify(commentRepository)
+                .deleteComment(
+                        10L,
+                        1L
+                );
+
+        verify(postService)
+                .incrementOrDecrementPostCommentsCount(
+                        10L,
+                        false
+                );
     }
 
 }
